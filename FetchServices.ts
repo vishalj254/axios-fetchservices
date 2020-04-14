b11eab9d91e072942a161cc5449943eb31d86e67
vishalj254 <vishaljain2504@gmail.com>
2020-04-14 12:14:37 +0530

    initial

---
+++ FetchServices.ts
@@ -0,0 +1,54 @@
+import axios from 'axios';
+const BaseURL='http://localhost:3000'
+
+const postData=async(url: String,body: Object)=>{
+  try {
+    console.log("call")
+    const response=await fetch(`${BaseURL}/${url}`,{
+      method:'post',
+      mode:'cors',
+      body:JSON.stringify(body),
+      headers:{'content-type':"application/json;charset=utf-8"}
+    })
+    var result=await response.json()
+    return result
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const postDataAxios=async(url:String,body:Object,config:Object={'content-type':"application/json;charset=utf-8"})=>{
+  try {
+    console.log(config)
+    var response=await axios.post(`${BaseURL}/${url}`,body,config)
+    console.log(response)
+    var result=response.data
+    console.log(result)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const getDataAxios=async(url:String,config:Object={'content-type':"application/json;charset:utf-8"})=>{
+  try {
+    var response=await axios.get(`${BaseURL}/${url}`,config)
+    var result=response.data
+    console.log(result)
+    return result
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const  postDataAndImage=async(url:String,formData:Object,config:Object={headers:{'content-type':'multipart/form-data'}})=>{
+  try{
+   const response=await axios.post(`${BaseURL}/${url}`,formData,config)
+    var result=response.data[0].RESULT
+    return(result)
+  }catch(e)
+  {
+   return(false)
+  }
+}
+
+export {BaseURL,postData,postDataAxios,getDataAxios,postDataAndImage}
